5cace7915cd5b6f2e118d20ae0d021832fa36e56
aimers313 <120925424+aimer313@users.noreply.github.com>
2025-01-18 22:24:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ app.use(cors({
     // ctrl + click to explore more
     credentials : true
 }))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/users" , userRouter) // good practise for defining api 
 
 
 // named export 
